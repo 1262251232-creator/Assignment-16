20b20dd25a7c6422046f340f8740f782a1801707
Prakhar Joshi <1262251232@mitwpu.edu.in>
2026-04-23 10:25:38 +0530

    Update Assignment 16.c

--- Assignment 16.c
+++ Assignment 16.c
@@ -5,7 +5,7 @@ int main() {
     printf("Enter number of elements (n): ");
     scanf("%d", &n);
 
-    int arr[n]; // Array declaration
+    int arr[n]; 
 
     for (i = 0; i < n; i++) {
         printf("Enter integer for arr[%d]: ", i);
